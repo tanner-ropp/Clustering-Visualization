ebdfa645d58636ea53d3eb7ed90117945de3217c
Tanner Ropp <tanner.ropp@ufl.edu>
2020-05-14 18:36:58 -0400

    added visualization folder

---
+++ src/ClusteringVisualizer/ClusteringVisualizer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class ClusteringVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                HEYOOOOO LEARN ABOUT CANVAS TAGS NEXT
+            </div>
+        )
+    }
+}
