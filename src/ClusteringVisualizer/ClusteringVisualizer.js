a7789beab6fbfa324aba4d3f02d2f0aadf4ba9b5
Tanner Ropp <tanner.ropp@ufl.edu>
2020-05-18 23:17:36 -0400

    added data point container

--- src/ClusteringVisualizer/ClusteringVisualizer.js
+++ src/ClusteringVisualizer/ClusteringVisualizer.js
@@ -12,20 +12,38 @@ export default class ClusteringVisualizer extends Component {
         const ctx = canvas.getContext('2d');
         console.log("Canvas mounted");
 
+        // Generate data points
+
+        var data_points = [];
+
+        for (let i = 0; i < 10; i++) {
+            data_points.push({
+                x: (Math.random() * 250) + 10,
+                y: (Math.random() * 250) + 10
+            });
+        }
+
+        for (let i = 0; i < 10; i++) {
+            data_points.push({
+                x: 740 - (Math.random() * 300),
+                y: (Math.random() * 250) + 10
+            });
+        }
+
+        for (let i = 0; i < 10; i++) {
+            data_points.push({
+                x: 250 + (Math.random() * 250),
+                y: 740 - (Math.random() * 300)
+            });
+        }
+
+        // Draw data points
+
         ctx.fillStyle = "#000000";
 
-        for (let i = 0; i < 10; i++) {
-            ctx.fillRect((Math.random() * 250) + 10, (Math.random() * 250) + 10, 10, 10);
-        }
-
-        for (let i = 0; i < 10; i++) {
-            ctx.fillRect(740 - (Math.random() * 300), (Math.random() * 250) + 10, 10, 10);
-        }
-
-        for (let i = 0; i < 10; i++) {
-            ctx.fillRect(250 + (Math.random() * 250), 740 - (Math.random() * 300), 10, 10);
-        }
-
+        data_points.forEach((item, i) => {
+            ctx.fillRect(item.x, item.y, 10, 10);
+        });
     }
 
     render() {
