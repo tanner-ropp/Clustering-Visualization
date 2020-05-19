c45a0323fb10727f0d73c3b73e07d1a4bc205c27
Tanner Ropp <tanner.ropp@ufl.edu>
2020-05-18 21:49:49 -0400

    added style sheet and test clusters

--- src/ClusteringVisualizer/ClusteringVisualizer.js
+++ src/ClusteringVisualizer/ClusteringVisualizer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './ClusteringVisualizer.css';
 
 export default class ClusteringVisualizer extends Component {
     constructor(props) {
@@ -6,10 +7,33 @@ export default class ClusteringVisualizer extends Component {
         this.state = {};
     }
 
+    componentDidMount() {
+        const canvas = document.getElementById('myCanvas');
+        const ctx = canvas.getContext('2d');
+        console.log("Canvas mounted");
+
+        ctx.fillStyle = "#000000";
+
+        for (let i = 0; i < 10; i++) {
+            ctx.fillRect((Math.random() * 250) + 10, (Math.random() * 250) + 10, 10, 10);
+        }
+
+        for (let i = 0; i < 10; i++) {
+            ctx.fillRect(740 - (Math.random() * 300), (Math.random() * 250) + 10, 10, 10);
+        }
+
+        for (let i = 0; i < 10; i++) {
+            ctx.fillRect(250 + (Math.random() * 250), 740 - (Math.random() * 300), 10, 10);
+        }
+
+    }
+
     render() {
         return (
             <div>
-                HEYOOOOO LEARN ABOUT CANVAS TAGS NEXT
+                <canvas id="myCanvas" width="750" height="750">
+                    Your browser does not support the canvas tag.
+                </canvas>
             </div>
         )
     }
